894353799440bdabebae14549d7b94a350fa9078
ManliestBen <benjamintmanley@gmail.com>
2019-09-26 13:47:23 -0500

    first commit

---
+++ src/components/titles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = (props) => {
+    return (
+        <div>
+            <h1>Weather API</h1>
+            <p>This app shows weather conditions for a city selected by the user.</p>
+        </div>
+    )
+}
+
+export default Titles;
---
+++ src/components/weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return (
+        <div>
+            <p>weather goes here</p>
+        </div>
+    )
+}
+
+export default Weather;
