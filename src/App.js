7c406f072ddfd15b606422b6c0ab1bb453363efc
ManliestBen <benjamintmanley@gmail.com>
2019-09-26 14:55:12 -0500

    change k to f

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import Form from '../src/components/form';
 import Weather from '../src/components/weather';
 
 class App extends React.Component {
-  
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`);
     const response = await api_call.json();
+    this.setState({
+      temperature: ((response.main.temp - 273.15)*(9/5)+32),
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      error: ""
+      
+    })
     console.log(response);
   }
   render() {
@@ -18,7 +34,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form loadWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     )
   }
